c86a618bf2708a6c5de48dc72859be0f82b90cd0
Kosta <68782786+KostaD02@users.noreply.github.com>
2023-06-06 17:58:25 +0400

    added meta tags

--- src/app/consts/projects.ts
+++ src/app/consts/projects.ts
@@ -2,16 +2,16 @@ import { InformationInterface } from '../interfaces';
 
 export const ProjectsInformation: InformationInterface[] = [
   {
-    name: "registry_imdo",
-    details: 5,
-    link: "https://registryimdo.org",
-    backgroundImageURL: "url('../../assets/images/registry_imdo.PNG')",
-  },
-  {
     name: "iswavle",
     details: 7,
     link: "https://iswavle.com",
     backgroundImageURL: "url('../../assets/images/iswavle.PNG')",
+  },
+  {
+    name: "registry_imdo",
+    details: 5,
+    link: "https://registryimdo.org",
+    backgroundImageURL: "url('../../assets/images/registry_imdo.PNG')",
   },
   {
     name: "qr_code",
