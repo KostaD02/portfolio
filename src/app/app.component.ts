15485f95f30ee57e07c9f7c51c499083aa16b9b2
Kosta <68782786+KostaD02@users.noreply.github.com>
2023-06-13 22:23:40 +0400

    adjust animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import * as AOS from 'aos';
 export class AppComponent implements OnInit, OnDestroy {
   private readonly destroy$ = new Subject<void>();
 
-  public isAnimationOn: boolean = false;
+  public isAnimationOn: boolean = localStorage.getItem("enable_animation") ? false : true || true;
 
   public hideEventEmitter: EventEmitter<boolean> = new EventEmitter();
   public startedScrollingEventEmitter: EventEmitter<boolean> = new EventEmitter();
