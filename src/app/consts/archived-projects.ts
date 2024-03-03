30a153a7b0b31c42f4379730ee54922756f295c3
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-03-03 16:50:31 +0400

    docs: update docs

--- src/app/consts/archived-projects.ts
+++ src/app/consts/archived-projects.ts
@@ -2,289 +2,364 @@ import { ArchivedProjectsInterface } from '../interfaces';
 
 export const ArchivedProjectsInformation: ArchivedProjectsInterface[] = [
   {
-    year: 2023,
-    title: "EverREST - learn Backend",
-    built_with: ["Nest.js", "MongoDB", "mdBook", "socket.io", "JWT"],
-    made_at: "Educata",
+    year: 2024,
+    title: 'Iswavle',
+    made_at: 'Educata',
+    built_with: ['Angular', 'Ng-Zorro', 'RxJS', 'SSR', 'SSG', 'md'],
     link: {
-      external: "https://everrest.educata.dev/",
-      github: "https://github.com/educata/everrest",
-      swagger: "https://api.everrest.educata.dev/swagger"
-    }
+      external: 'https://iswavle.com',
+      github: 'https://github.com/educata/iswavle',
+    },
+  },
+  {
+    year: 2024,
+    title: 'ShopLoyalty',
+    built_with: [
+      'Angular',
+      'Angular material',
+      'SCSS',
+      'RxJS',
+      'SweetAlert2',
+      'Nest.js',
+      'MongoDB',
+      'JWT',
+    ],
+    link: {
+      external: 'https://shoployalty.konstantinedatunishvili.com/',
+      github: 'https://github.com/KostaD02/ShopLoyalty',
+      swagger:
+        'https://backend-shoployalty.konstantinedatunishvili.com/swagger',
+    },
   },
   {
     year: 2023,
-    title: "Kvlividze - Learn georgian",
-    made_at: "Freelance",
-    built_with: ["Angular", "Angular material", "SCSS", "RxJS", "SweetAlert2", "i18n", "Express", "MongoDB", "JWT"],
+    title: 'EverREST - learn Backend',
+    built_with: ['Nest.js', 'MongoDB', 'mdBook', 'socket.io', 'JWT'],
+    made_at: 'Educata',
     link: {
-      external: "https://kvlividze.com"
-    }
+      external: 'https://everrest.educata.dev/',
+      github: 'https://github.com/educata/everrest',
+      swagger: 'https://api.everrest.educata.dev/swagger',
+    },
   },
   {
     year: 2023,
-    title: "Iswavle - Educational website",
-    built_with: ["Angular", "Angular material", "SCSS", "RxJS", "SSR", "SweetAlert2", "Express", "MongoDB"],
+    title: 'Kvlividze - Learn georgian',
+    made_at: 'Freelance',
+    built_with: [
+      'Angular',
+      'Angular material',
+      'SCSS',
+      'RxJS',
+      'SweetAlert2',
+      'i18n',
+      'Express',
+      'MongoDB',
+      'JWT',
+    ],
     link: {
-      external: "https://iswavle.com",
-      github: "https://github.com/KostaD02/iswavle"
-    }
+      external: 'https://kvlividze.com',
+    },
   },
   {
     year: 2023,
-    title: "Realtime chat",
-    built_with: ["Angular", "SCSS", "Firebase"],
+    title: 'Iswavle - Educational website',
+    built_with: [
+      'Angular',
+      'Angular material',
+      'SCSS',
+      'RxJS',
+      'SSR',
+      'SweetAlert2',
+      'Express',
+      'MongoDB',
+    ],
     link: {
-      external: "https://chat.konstantinedatunishvili.com/",
-      github: "https://github.com/KostaD02/Realtime-Chat"
-    }
+      external: 'https://iswavle.web.app',
+      github: 'https://github.com/KostaD02/iswavle',
+    },
   },
   {
     year: 2023,
-    title: "QR Read & Write",
-    built_with: ["Angular", "SCSS", "SweetAlert2"],
+    title: 'Realtime chat',
+    built_with: ['Angular', 'SCSS', 'Firebase'],
     link: {
-      external: "https://qr.konstantinedatunishvili.com/",
-      github: "https://kostad02.github.io/QR-read-write"
-    }
+      external: 'https://chat.konstantinedatunishvili.com/',
+      github: 'https://github.com/KostaD02/Realtime-Chat',
+    },
   },
   {
     year: 2023,
-    title: "QA exercise",
-    built_with: ["Angular", "Angular material", "SCSS", "Firebase", "SweetAlert2"],
+    title: 'QR Read & Write',
+    built_with: ['Angular', 'SCSS', 'SweetAlert2'],
     link: {
-      external: "https://qa.konstantinedatunishvili.com/",
-      github: "https://github.com/KostaD02/QA-Exercise"
-    }
+      external: 'https://qr.konstantinedatunishvili.com/',
+      github: 'https://kostad02.github.io/QR-read-write',
+    },
   },
   {
     year: 2023,
-    title: "Payever components",
-    made_at: "Payever",
-    built_with: ["Angular", "SCSS", "RxJS"],
+    title: 'QA exercise',
+    built_with: [
+      'Angular',
+      'Angular material',
+      'SCSS',
+      'Firebase',
+      'SweetAlert2',
+    ],
     link: {
-      external: "https://commerceos.payever.org"
-    }
+      external: 'https://qa.konstantinedatunishvili.com/',
+      github: 'https://github.com/KostaD02/QA-Exercise',
+    },
   },
   {
     year: 2023,
-    title: "Ravillas apartments",
-    made_at: "Freelance",
-    built_with: ["Angular", "Angular material", "SCSS", "Firebase", "SweetAlert2"]
+    title: 'Payever components',
+    made_at: 'Payever',
+    built_with: ['Angular', 'SCSS', 'RxJS'],
+    link: {
+      external: 'https://commerceos.payever.org',
+    },
   },
   {
     year: 2023,
-    title: "Sliding Puzzle game",
-    built_with: ["HTML", "CSS", "JS", "SweetAlert2"],
+    title: 'Ravillas apartments',
+    made_at: 'Freelance',
+    built_with: [
+      'Angular',
+      'Angular material',
+      'SCSS',
+      'Firebase',
+      'SweetAlert2',
+    ],
+  },
+  {
+    year: 2023,
+    title: 'Sliding Puzzle game',
+    built_with: ['HTML', 'CSS', 'JS', 'SweetAlert2'],
     link: {
-      external: "https://kostad02.github.io/Sliding-Puzzle-Game-JS",
-      github: "https://github.com/KostaD02/Sliding-Puzzle-Game-JS"
-    }
+      external: 'https://kostad02.github.io/Sliding-Puzzle-Game-JS',
+      github: 'https://github.com/KostaD02/Sliding-Puzzle-Game-JS',
+    },
   },
   {
     year: 2022,
-    title: "Memory card game",
-    built_with: ["HTML", "CSS", "JS", "SweetAlert2"],
+    title: 'Memory card game',
+    built_with: ['HTML', 'CSS', 'JS', 'SweetAlert2'],
     link: {
-      external: "https://kostad02.github.io/Memory-Card-Game-JS",
-      github: "https://github.com/KostaD02/Memory-Card-Game-JS"
-    }
+      external: 'https://kostad02.github.io/Memory-Card-Game-JS',
+      github: 'https://github.com/KostaD02/Memory-Card-Game-JS',
+    },
   },
   {
     year: 2022,
-    title: "Color game",
-    built_with: ["HTML", "CSS", "JS", "SweetAlert2"],
+    title: 'Color game',
+    built_with: ['HTML', 'CSS', 'JS', 'SweetAlert2'],
     link: {
-      external: "https://kostad02.github.io/Color-Game-JS",
-      github: "https://github.com/KostaD02/Color-Game-JS"
-    }
+      external: 'https://kostad02.github.io/Color-Game-JS',
+      github: 'https://github.com/KostaD02/Color-Game-JS',
+    },
   },
   {
     year: 2022,
-    title: "Registry imdo",
-    made_at: "Freelance",
-    built_with: ["Angular", "Angular material", "SweetAlert2", "Express", "MongoDB"],
+    title: 'Registry imdo',
+    made_at: 'Freelance',
+    built_with: [
+      'Angular',
+      'Angular material',
+      'SweetAlert2',
+      'Express',
+      'MongoDB',
+    ],
     link: {
-      external: "https://registryimdo.org"
-    }
+      external: 'https://registryimdo.org',
+    },
   },
   {
     year: 2022,
-    title: "QR Project",
-    made_at: "University",
-    built_with: ["Angular", "Angular material", "SweetAlert2", "Express", "MongoDB"],
+    title: 'QR Project',
+    made_at: 'University',
+    built_with: [
+      'Angular',
+      'Angular material',
+      'SweetAlert2',
+      'Express',
+      'MongoDB',
+    ],
     link: {
-      article: "qr_project"
-    }
+      article: 'qr_project',
+    },
   },
   {
     year: 2022,
-    title: "QR Project landing page",
-    made_at: "University",
-    built_with: ["HTML", "CSS", "bootstrap", "JS"],
+    title: 'QR Project landing page',
+    made_at: 'University',
+    built_with: ['HTML', 'CSS', 'bootstrap', 'JS'],
     link: {
-      external: "https://kostad02.github.io/QR-Project/",
-      github: "https://github.com/KostaD02/QR-Project"
-    }
+      external: 'https://kostad02.github.io/QR-Project/',
+      github: 'https://github.com/KostaD02/QR-Project',
+    },
   },
   {
     year: 2021,
-    title: "Simple CRUD for Authentication",
-    built_with: ["Node.js", "Express", "MongoDB", "CORS"],
+    title: 'Simple CRUD for Authentication',
+    built_with: ['Node.js', 'Express', 'MongoDB', 'CORS'],
     link: {
       // github: "https://github.com/KostaD02/AuthApiTest",
-      swagger: "https://app.swaggerhub.com/apis-docs/KostaD02/AuthSimpleApi/0.1/"
-    }
+      swagger:
+        'https://app.swaggerhub.com/apis-docs/KostaD02/AuthSimpleApi/0.1/',
+    },
   },
   {
     year: 2021,
-    title: "Quiz me",
-    built_with: ["Angular", "Angular material", "SweetAlert2", "Firebase"],
+    title: 'Quiz me',
+    built_with: ['Angular', 'Angular material', 'SweetAlert2', 'Firebase'],
     link: {
-      external: "https://quizme-3318d.web.app",
-      github: "https://github.com/KostaD02/quizme"
-    }
+      external: 'https://quizme-3318d.web.app',
+      github: 'https://github.com/KostaD02/quizme',
+    },
   },
   {
     year: 2021,
-    title: "Binary Tree cpp",
-    built_with: ["C++"],
+    title: 'Binary Tree cpp',
+    built_with: ['C++'],
     link: {
-      github: "https://github.com/KostaD02/Binary-Tree-cpp"
-    }
+      github: 'https://github.com/KostaD02/Binary-Tree-cpp',
+    },
   },
   {
     year: 2021,
-    title: "Data structure cpp",
-    built_with: ["C++"],
+    title: 'Data structure cpp',
+    built_with: ['C++'],
     link: {
-      github: "https://github.com/KostaD02/Data-structure-CPP"
-    }
+      github: 'https://github.com/KostaD02/Data-structure-CPP',
+    },
   },
   {
     year: 2021,
-    title: "Comment with Firebase",
-    built_with: ["HTML", "CSS", "JS", "Firebase"],
+    title: 'Comment with Firebase',
+    built_with: ['HTML', 'CSS', 'JS', 'Firebase'],
     link: {
-      github: "https://github.com/KostaD02/Comment-with-firebase",
-      external: "https://kostad02.github.io/Comment-with-firebase"
-    }
+      github: 'https://github.com/KostaD02/Comment-with-firebase',
+      external: 'https://kostad02.github.io/Comment-with-firebase',
+    },
   },
   {
     year: 2021,
-    title: "Travel More (non responsive)",
-    built_with: ["Angular", "Bootstrap", "Firebase"],
+    title: 'Travel More (non responsive)',
+    built_with: ['Angular', 'Bootstrap', 'Firebase'],
     link: {
-      github: "https://github.com/KostaD02/TravelMore-angular",
-      external: "https://travelmore-a0675.firebaseapp.com"
-    }
+      github: 'https://github.com/KostaD02/TravelMore-angular',
+      external: 'https://travelmore-a0675.firebaseapp.com',
+    },
   },
   {
     year: 2021,
-    title: "Tower of hanoi",
-    built_with: ["C++"],
+    title: 'Tower of hanoi',
+    built_with: ['C++'],
     link: {
-      external: "https://github.com/KostaD02/Tower-of-Hanoi"
-    }
+      external: 'https://github.com/KostaD02/Tower-of-Hanoi',
+    },
   },
   {
     year: 2021,
-    title: "Form builder",
-    built_with: ["Angular", "Firebase"],
+    title: 'Form builder',
+    built_with: ['Angular', 'Firebase'],
     link: {
-      github: "https://github.com/KostaD02/form-builder/tree/master/angularForm",
-      external: "https://angularformworking.web.app/"
-    }
+      github:
+        'https://github.com/KostaD02/form-builder/tree/master/angularForm',
+      external: 'https://angularformworking.web.app/',
+    },
   },
   {
     year: 2020,
-    title: "Bigint class",
-    built_with: ["C++"],
+    title: 'Bigint class',
+    built_with: ['C++'],
     link: {
-      github: "https://github.com/KostaD02/BigInt-Class"
-    }
+      github: 'https://github.com/KostaD02/BigInt-Class',
+    },
   },
   {
     year: 2020,
-    title: "Spiral color change",
-    built_with: ["HTML", "CSS", "JS"],
+    title: 'Spiral color change',
+    built_with: ['HTML', 'CSS', 'JS'],
     link: {
-      github: "https://github.com/KostaD02/Spiral-color-change-js",
-      external: "https://kostad02.github.io/Spiral-color-change-js/"
-    }
+      github: 'https://github.com/KostaD02/Spiral-color-change-js',
+      external: 'https://kostad02.github.io/Spiral-color-change-js/',
+    },
   },
   {
     year: 2020,
-    title: "Tic Tac Toe",
-    built_with: ["HTML", "CSS", "JS"],
+    title: 'Tic Tac Toe',
+    built_with: ['HTML', 'CSS', 'JS'],
     link: {
-      github: "https://github.com/KostaD02/Tic-Tac-Toe-JS",
-      external: "https://kostad02.github.io/Tic-Tac-Toe-JS"
-    }
+      github: 'https://github.com/KostaD02/Tic-Tac-Toe-JS',
+      external: 'https://kostad02.github.io/Tic-Tac-Toe-JS',
+    },
   },
   {
     year: 2020,
-    title: "Movie CRUD example",
-    built_with: ["React", "lodash"],
+    title: 'Movie CRUD example',
+    built_with: ['React', 'lodash'],
     link: {
-      github: "https://github.com/KostaD02/React-movie-example"
-    }
+      github: 'https://github.com/KostaD02/React-movie-example',
+    },
   },
   {
     year: 2020,
-    title: "CRUD Blog",
-    built_with: ["Express", "MongoDB"],
+    title: 'CRUD Blog',
+    built_with: ['Express', 'MongoDB'],
     link: {
-      github: "https://github.com/KostaD02/SimpleBlogSite"
-    }
+      github: 'https://github.com/KostaD02/SimpleBlogSite',
+    },
   },
   {
     year: 2020,
-    title: "Simple Calculator",
-    built_with: ["React"],
+    title: 'Simple Calculator',
+    built_with: ['React'],
     link: {
-      github: "https://github.com/KostaD02/React-Simple-Calculator"
-    }
+      github: 'https://github.com/KostaD02/React-Simple-Calculator',
+    },
   },
   {
     year: 2020,
-    title: "Rational number class",
-    built_with: ["C++"],
+    title: 'Rational number class',
+    built_with: ['C++'],
     link: {
-      github: "https://github.com/KostaD02/Rational-Number-Class"
-    }
+      github: 'https://github.com/KostaD02/Rational-Number-Class',
+    },
   },
   {
     year: 2020,
-    title: "Date and time class",
-    built_with: ["C++"],
+    title: 'Date and time class',
+    built_with: ['C++'],
     link: {
-      github: "https://github.com/KostaD02/Date-and-Time"
-    }
+      github: 'https://github.com/KostaD02/Date-and-Time',
+    },
   },
   {
     year: 2020,
-    title: "Convert number to word",
-    built_with: ["C++"],
+    title: 'Convert number to word',
+    built_with: ['C++'],
     link: {
-      github: "https://github.com/KostaD02/Convert-Number-To-Word"
-    }
+      github: 'https://github.com/KostaD02/Convert-Number-To-Word',
+    },
   },
   {
     year: 2020,
-    title: "Movie Theater",
-    built_with: ["HTML", "CSS"],
+    title: 'Movie Theater',
+    built_with: ['HTML', 'CSS'],
     link: {
-      external: "https://kostad02.github.io/MovieTheater/",
-      github: "https://github.com/KostaD02/MovieTheater"
-    }
+      external: 'https://kostad02.github.io/MovieTheater/',
+      github: 'https://github.com/KostaD02/MovieTheater',
+    },
   },
   {
     year: 2020,
-    title: "Student data convertor",
-    built_with: ["C++"],
+    title: 'Student data convertor',
+    built_with: ['C++'],
     link: {
-      github: "https://github.com/KostaD02/StudentsBase"
-    }
-  }
+      github: 'https://github.com/KostaD02/StudentsBase',
+    },
+  },
 ];
